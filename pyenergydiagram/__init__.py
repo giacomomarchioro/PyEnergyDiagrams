99d87398d724cbcf0c7918c479f335ee5c10431d
giacomo <giacomomarchioro@outlook.com>
2020-05-06 15:59:12 +0200

    Added init file and setup

---
+++ pyenergydiagram/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 12:56:47 2019
+
+@author: giacomo
+"""
+from .energydiagram import ED
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyenergydiagram",
+    version="0.0.1",
+    author="Giacomo Marchioro",
+    author_email="giacomomarchioro@outlook.com",
+    description="A tool for plotting Energy Diagrams using Matplotlib.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/giacomomarchioro/PyEnergyDiagrams",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
