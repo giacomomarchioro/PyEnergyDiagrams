7eaef8e7184201d00c1ed6cc8ecacd03c1899a5a
ProodjePindakaas <f.vdbosch98@gmail.com>
2023-04-14 12:05:20 +0200

    error box notation

--- energydiagram/energydiagram.py
+++ energydiagram/energydiagram.py
@@ -15,7 +15,7 @@ y|
 """
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
-from box_notation import plot_orbital_boxes
+from .box_notation import plot_orbital_boxes
 
 
 class ED:
